98e97a510fe92f7a6045f4927980858540f30f53
nadiannis <annisanadianeyla@gmail.com>
2020-07-26 07:20:01 +0700

    Make the menu button works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,14 @@
 import '../styles/main.sass';
+
+const menu = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('nav-show');
+});
+
+document.addEventListener('click', e => {
+  if (e.target.closest('.nav')) return; 
+
+  nav.classList.remove('nav-show');
+});
